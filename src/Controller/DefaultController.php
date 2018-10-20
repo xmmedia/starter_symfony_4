21af0eef0ba5b05fa8475bf65c9c42fffceed64f
Darryl Hein <dhein@xmmedia.com>
2018-10-20 14:49:00 -0600

    add return types

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,13 +6,14 @@ use Knp\Component\Pager\PaginatorInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends AbstractController
 {
     /**
      * @Route("/", name="index")
      */
-    public function indexAction(Request $request)
+    public function indexAction(Request $request): Response
     {
         return $this->render('default/index.html.twig');
     }
@@ -20,7 +21,7 @@ class DefaultController extends AbstractController
     /**
      * @Route("/pattern-library-public", name="pattern_library_public")
      */
-    public function patternLibraryPublicAction(PaginatorInterface $paginator)
+    public function patternLibraryPublicAction(PaginatorInterface $paginator): Response
     {
         $pagination = $paginator->paginate(
             range(1, 10), /* some random data */
@@ -36,7 +37,7 @@ class DefaultController extends AbstractController
     /**
      * @Route("/pattern-library-admin", name="pattern_library_admin")
      */
-    public function patternLibraryAdminAction(PaginatorInterface $paginator)
+    public function patternLibraryAdminAction(PaginatorInterface $paginator): Response
     {
         $pagination = $paginator->paginate(
             range(1, 10), /* some random data */
