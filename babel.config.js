8bdc265aedaf1beef567125717067478ba85327d
Darryl Hein <dhein@xmmedia.com>
2018-11-15 21:44:24 -0700

    remove old babel config

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,3 @@
-// module.exports = {
-//     "presets": [
-//         ["@babel/preset-env", { "modules": false, "useBuiltIns": "usage" }],
-//     ],
-//     "plugins": ["@babel/plugin-syntax-dynamic-import"],
-//     "comments": false
-// };
-
-
 module.exports = {
     presets: [
         '@vue/app',
