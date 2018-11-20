13f53f2923fcb8ec664f38f3717e95efc4e8719b
Darryl Hein <dhein@xmmedia.com>
2018-11-19 21:39:44 -0700

    email VO

---
+++ src/Model/Email.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model;
+
+final class Email implements ValueObject
+{
+    /** @var string */
+    private $email;
+
+    /** @var string|null */
+    private $name;
+
+    public static function fromString(string $email, string $name = null): self
+    {
+        return new self($email, $name);
+    }
+
+    private function __construct(string $email, string $name = null)
+    {
+        $this->email = $email;
+        $this->name = $name;
+    }
+
+    public function email(): string
+    {
+        return $this->email;
+    }
+
+    public function name(): ?string
+    {
+        return $this->name;
+    }
+
+    public function toString(): string
+    {
+        return $this->email;
+    }
+
+    public function __toString(): string
+    {
+        return $this->toString();
+    }
+
+    public function withName(): string
+    {
+        if (null === $this->name) {
+            return $this->email;
+        }
+
+        return sprintf('%s <%s>', $this->name, $this->email);
+    }
+
+    /**
+     * @param Email|ValueObject $other
+     */
+    public function sameAs(ValueObject $other): bool
+    {
+        return get_class($this) === get_class($other) && strtolower($this->email) === strtolower($other->email);
+    }
+}
---
+++ tests/Model/EmailTest.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Model;
+
+use App\Model\Email;
+use Faker;
+use Mockery;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use PHPUnit\Framework\TestCase;
+
+class EmailTest extends TestCase
+{
+    use MockeryPHPUnitIntegration;
+
+    public function testFromString(): void
+    {
+        $faker = Faker\Factory::create();
+
+        $email = $faker->email;
+        $name = $faker->name;
+
+        $vo = Email::fromString($email, $name);
+
+        $this->assertEquals($email, $vo->email());
+        $this->assertEquals($email, $vo->toString());
+        $this->assertEquals($email, (string) $vo);
+        $this->assertEquals($name, $vo->name());
+    }
+
+    public function testFromStringWithoutName(): void
+    {
+        $faker = Faker\Factory::create();
+
+        $email = $faker->email;
+
+        $vo = Email::fromString($email);
+
+        $this->assertEquals($email, $vo->toString());
+        $this->assertEquals($email, (string) $vo);
+        $this->assertNull($vo->name());
+    }
+
+    public function testFromStringWithNameMethod(): void
+    {
+        $vo = Email::fromString('email@email.com', 'Name');
+
+        $this->assertEquals('Name <email@email.com>', $vo->withName());
+    }
+
+    public function testFromStringWithNameMethodWithoutName(): void
+    {
+        $vo = Email::fromString('email@email.com');
+
+        $this->assertEquals('email@email.com', $vo->withName());
+    }
+
+    public function testSameAs(): void
+    {
+        $vo1 = Email::fromString('email@email.com');
+        $vo2 = Email::fromString('email@email.com');
+
+        $this->assertTrue($vo1->sameAs($vo2));
+    }
+
+    public function testSameAsCapitals(): void
+    {
+        $vo1 = Email::fromString('eMail@email.com');
+        $vo2 = Email::fromString('email@eMail.com');
+
+        $this->assertTrue($vo1->sameAs($vo2));
+    }
+}
