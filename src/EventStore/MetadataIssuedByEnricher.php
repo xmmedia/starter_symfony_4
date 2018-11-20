11e7f4cd276813448a479c2bdf0708416eba0327
Darryl Hein <dhein@xmmedia.com>
2018-11-20 14:48:48 -0700

    simplify event issued by metadata enricher

---
+++ src/EventStore/MetadataIssuedByEnricher.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\EventStore;
+
+use App\DataProvider\IssuerProvider;
+use Prooph\Common\Messaging\Message;
+use Prooph\EventStore\Metadata\MetadataEnricher;
+
+class MetadataIssuedByEnricher implements MetadataEnricher
+{
+    /** @var IssuerProvider */
+    private $issuerProvider;
+
+    public function __construct(IssuerProvider $issuerProvider)
+    {
+        $this->issuerProvider = $issuerProvider;
+    }
+
+    public function enrich(Message $message): Message
+    {
+        return $message->withAddedMetadata('issuedBy', $this->issuerProvider->getIssuer());
+    }
+}
