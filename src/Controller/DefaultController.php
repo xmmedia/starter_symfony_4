bc8efa3f44996abc7476d020e1a2a7b393d7585b
Darryl Hein <dhein@xmmedia.com>
2018-11-19 21:31:33 -0700

    ignore code coverage on controller

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -10,6 +10,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @codeCoverageIgnore
+ */
 class DefaultController extends AbstractController
 {
     /**
