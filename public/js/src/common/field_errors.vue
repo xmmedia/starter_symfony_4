e2186ef1016f43ce786875c8447600a0b55b1332
Darryl Hein <dhein@xmmedia.com>
2018-11-23 16:33:25 -0700

    support paths to field errors

--- public/js/src/common/field_errors.vue
+++ public/js/src/common/field_errors.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-import { get } from 'lodash';
+import { get, toPath } from 'lodash';
 
 export default {
     props: {
@@ -34,11 +34,34 @@ export default {
         },
 
         flatErrors () {
-            return get(this.errors, this.errorPath);
+            const errors = [];
+
+            const e = get(this.errors, this.errorPaths[0]);
+            if (e) {
+                errors.push(...e);
+            }
+
+            if (this.errorPaths.length > 1) {
+                const e = get(this.errors, this.errorPaths[1]);
+                if (e) {
+                    errors.push(...e);
+                }
+            }
+
+            return errors;
         },
 
-        errorPath () {
-            return ['children', this.field, 'errors'];
+        errorPaths () {
+            const path = toPath(this.field);
+
+            if (path.length === 1) {
+                return [['children', this.field, 'errors']];
+            }
+
+            return [
+                ['children', path[0], 'errors'],
+                ['children', path[0], 'children', path[1], 'errors'],
+            ];
         },
     },
 };
