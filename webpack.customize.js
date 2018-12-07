dc559f57fa6c232e21795e222dd40f9a04b2ec1f
Darryl Hein <dhein@xmmedia.com>
2018-12-07 11:47:38 -0700

    minor webpack config cleanup

--- webpack.customize.js
+++ webpack.customize.js
@@ -1,6 +1,4 @@
 'use strict';
-const path = require('path');
-
 // Tweak the webpack config output of Encore
 // Use as a common place for customizations that apply to all webpack envs.
 module.exports = {
