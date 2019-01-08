8ffd74baf42e65f3121d2d0909a86ecc0535f12a
Darryl Hein <dhein@xmmedia.com>
2019-01-08 15:18:22 -0700

    voter trait

---
+++ src/Security/Voter/IsAdminTrait.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Security\Voter;
+
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+
+/**
+ * @property \Symfony\Component\Security\Core\Authorization\AccessDecisionManagerInterface $decisionManager
+ */
+trait IsAdminTrait
+{
+    /**
+     * True when the user has an admin role.
+     */
+    protected function isAdmin(TokenInterface $token)
+    {
+        return $this->decisionManager->decide($token, ['ROLE_ADMIN']);
+    }
+}
