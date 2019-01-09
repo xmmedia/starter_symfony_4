40816753ba904aea8e3368318dbe9856a595559f
Darryl Hein <dhein@xmmedia.com>
2019-01-09 16:28:38 -0700

    remove apollo boost / switch to custom init
    boost didn’t work and didn’t allow a connection to the dev tools

--- public/js/src/common/apollo.js
+++ public/js/src/common/apollo.js
@@ -1,9 +1,23 @@
-import ApolloClient from 'apollo-boost';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import VueApollo from 'vue-apollo';
 
-const apolloClient = new ApolloClient({
-    uri: 'https://dev.example.com/graphql',
+// HTTP connexion to the API
+const httpLink = new HttpLink({
+    uri: process.env.REQUEST_CONTEXT_SCHEME+'://'+process.env.REQUEST_CONTEXT_HOST+'/graphql/',
+    connectToDevTools: process.env.NODE_ENV === 'production',
 });
+
+// Cache implementation
+const cache = new InMemoryCache();
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+    link: httpLink,
+    cache,
+});
+
 const apolloProvider = new VueApollo({
     defaultClient: apolloClient,
 });
