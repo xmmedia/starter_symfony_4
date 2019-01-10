4873edae0f4da886963ef4f2d034ec963aa9ad04
Darryl Hein <dhein@xmmedia.com>
2019-01-09 21:48:49 -0700

    add id field to user create form type

--- src/Form/AdminUserCreateType.php
+++ src/Form/AdminUserCreateType.php
@@ -15,6 +15,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -36,6 +37,10 @@ class AdminUserCreateType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('id', HiddenType::class, [
+                'label' => 'User ID',
+                // no constraints because we don't want to show these errors to the user
+            ])
             ->add('email', EmailType::class, [
                 'label'       => 'Email',
                 'attr'        => ['maxlength' => 150],
