b60519aa1a6893ac2de704978915f20efaa1fa98
Darryl Hein <dhein@xmmedia.com>
2019-02-12 09:52:43 -0700

    cleanup 404 component

--- public/js/src/admin/error/404.vue
+++ public/js/src/admin/error/404.vue
@@ -10,25 +10,3 @@
         </p>
     </div>
 </template>
-
-<script>
-export default {
-    components: {},
-
-    props: {},
-
-    data () {
-        return {};
-    },
-
-    computed: {},
-
-    watch: {},
-
-    beforeMount () {},
-
-    mounted () {},
-
-    methods: {},
-};
-</script>
