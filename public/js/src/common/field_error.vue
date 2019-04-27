756dc2f51b84c6aecf2f119efab6d5a35b5daa5f
Darryl Hein <dhein@xmmedia.com>
2019-04-26 21:04:51 -0600

    add field error component
    for later use

---
+++ public/js/src/common/field_error.vue
@@ -0,0 +1,15 @@
+<template>
+    <ul v-if="hasErrors" class="field-errors" role="alert" aria-live="polite">
+        <li><slot></slot></li>
+    </ul>
+</template>
+
+<script>
+export default {
+    computed: {
+        hasErrors () {
+            return !!this.$slots.default;
+        },
+    },
+}
+</script>
