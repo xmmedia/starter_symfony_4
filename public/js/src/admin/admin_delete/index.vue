4dca29328c1b75b4922144166e2bf70b09b7c7d7
Darryl Hein <dhein@xmmedia.com>
2019-04-26 20:31:46 -0600

    update admin delete for gql
    doesn’t actually delete, just shows warning & emits delete event

--- public/js/src/admin/admin_delete/index.vue
+++ public/js/src/admin/admin_delete/index.vue
@@ -1,64 +1,75 @@
-<!-- @todo may need work the first time it's used -->
 <template>
     <span>
-        <div>
-            <button class="form-action"
-                    @click="$modal.show('admin-delete')">Delete</button>
-        </div>
+        <button ref="link"
+                class="button-link form-action"
+                type="button"
+                @click="open">Delete</button>
 
         <portal to="modal">
             <modal :adaptive="true"
                    :scrollable="true"
                    name="admin-delete"
                    height="auto"
-                   transition="md">
+                   transition="md"
+                   @opened="opened">
                 <div slot="top-right" class="text-4xl pr-6">
                     <button class="hover:no-underline text-white"
                             type="button"
                             @click="close">×</button>
                 </div>
 
-                <div class="p-4 text-center">
-                    <p>Are you sure you want to delete this {{ recordDesc }}? This cannot be undone.</p>
-                    <div>
-                        <form :action="action" name="form" method="post">
-                            <input type="hidden" name="_method" value="DELETE">
-                            <input id="form__token" :value="csrfToken" type="hidden" name="form[_token]">
-                            <div>
-                                <button class="button bg-red-dark border-red-dark hover:bg-red hover:border-red">
-                                    Delete
-                                </button>
-                                <button class="form-action button-link" @click.prevent="close">Cancel</button>
-                            </div>
-                        </form>
+                <div v-if="!deleting" class="p-4 text-center">
+                    <div class="my-4">
+                        Are you sure you want to delete this {{ recordDesc }}?
+                        This cannot be undone.
+                    </div>
+                    <div class="mb-4">
+                        <button class="button bg-red-dark border-red-dark hover:bg-red hover:border-red"
+                                type="button"
+                                @click="deleteRecord">Delete</button>
+                        <button ref="cancel"
+                                class="form-action button-link"
+                                type="button"
+                                @click="close">Cancel</button>
                     </div>
                 </div>
+
+                <loading-spinner v-else class="p-8 text-center">Deleting {{ recordDesc }}...</loading-spinner>
             </modal>
         </portal>
     </span>
 </template>
 
 <script>
-export default {
-    props: {
-        action: {
-            type: String,
-            required: true,
+    export default {
+        props: {
+            recordDesc: {
+                type: String,
+                required: true,
+            },
         },
-        recordDesc: {
-            type: String,
-            required: true,
-        },
-        csrfToken: {
-            type: String,
-            required: true,
-        },
-    },
 
-    methods: {
-        close () {
-            this.$modal.hide('admin-delete');
+        data () {
+            return {
+                deleting: false,
+            };
         },
-    },
-}
+
+        methods: {
+            open () {
+                this.$modal.show('admin-delete');
+            },
+            opened () {
+                this.$refs.cancel.focus();
+            },
+            deleteRecord () {
+                this.deleting = true;
+                this.$emit('delete');
+            },
+            close () {
+                this.$modal.hide('admin-delete');
+                this.$refs.link.focus();
+            },
+        },
+    }
 </script>
