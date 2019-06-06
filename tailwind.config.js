a8a9cbc886b4c98bfdc3d54eb7ea3f1476cb7194
Herman Steuernagel <herman@thefourth.ca>
2019-06-06 13:10:21 -0700

    updating stylesheets for tailwind 1.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,22 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+        colors: {
+            'inherit': 'inherit',
+        },
+        borderWidth: {
+            '10': '10px',
+        },
+        maxWidth: {
+            '1/2': '50%',
+            '3/5': '60%',
+            '11/12': '91%',
+        },
+        screens: {
+            'xs': '400px',
+            'max': '1245px',
+        },
+    },
   },
   variants: {},
   plugins: []
