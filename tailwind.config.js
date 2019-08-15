77ac379efe491421109944ae69fc1d3e12a16431
Darryl Hein <dhein@xmmedia.com>
2019-08-15 15:52:05 -0600

    code formatting in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,29 @@
 module.exports = {
-  theme: {
-    extend: {
-        colors: {
-            'inherit': 'inherit',
-        },
-        borderWidth: {
-            '10': '10px',
-        },
-        maxWidth: {
-            '1/2': '50%',
-            '3/5': '60%',
-            '11/12': '91%',
-        },
-        screens: {
-            'xs': '400px',
-            'max': '1245px',
-            'print': { 'raw': 'print' },
-            'retina': { 'raw': '(-webkit-min-device-pixel-ratio: 2), (min-resolution: 192dpi)' },
+    theme: {
+        extend: {
+            colors: {
+                'inherit': 'inherit',
+            },
+            borderWidth: {
+                '10': '10px',
+            },
+            maxWidth: {
+                '1/2': '50%',
+                '3/5': '60%',
+                '11/12': '91%',
+            },
+            screens: {
+                'xs': '400px',
+                'max': '1245px',
+                'print': { 'raw': 'print' },
+                'retina': { 'raw': '(-webkit-min-device-pixel-ratio: 2), (min-resolution: 192dpi)' },
+            },
         },
     },
-  },
-  variants: {
-    textColor: ['group-hover'],
-    borderColor: ['group-hover'],
-    opacity: ['responsive', 'hover', 'focus'],
-  },
-  plugins: []
-}
+    variants: {
+        textColor: ['group-hover'],
+        borderColor: ['group-hover'],
+        opacity: ['responsive', 'hover', 'focus'],
+    },
+    plugins: [],
+};
