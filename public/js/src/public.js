d591d9ab87c11d53fab41420624eb6bb4e3536c9
Darryl Hein <dhein@xmmedia.com>
2019-08-15 22:55:48 -0600

    adding a bit of stuff to public.js

--- public/js/src/public.js
+++ public/js/src/public.js
@@ -2,12 +2,16 @@ import 'es6-promise/auto';
 
 import Vue from 'vue';
 import VueApollo from 'vue-apollo';
-import PortalVue from 'portal-vue';
+// import PortalVue from 'portal-vue';
 
 import apolloProvider from './common/apollo';
+// import * as filters from './common/filters';
 
+import loadingSpinner from './common/loading_spinner';
 import svgIcons from './common/svg_icons';
+import formError from './common/form_error';
 import fieldErrors from './common/field_errors';
+import fieldError from './common/field_error';
 
 // SASS/CSS
 import '../../css/public.scss';
@@ -19,16 +23,20 @@ import '../../css/public.scss';
 Vue.config.productionTip = false;
 
 Vue.use(VueApollo);
-Vue.use(PortalVue);
+// Vue.use(PortalVue);
 
+Vue.component('loading-spinner', loadingSpinner);
+Vue.component('form-error', formError);
 Vue.component('field-errors', fieldErrors);
+Vue.component('field-error', fieldError);
+Vue.component('local-time', () => import(/* webpackChunkName: "local-time" */ './common/local_time'));
 
 window.App = new Vue({
     el: '#app',
     apolloProvider,
 
     components: {
-        'svg-icons': svgIcons,
+        svgIcons,
         'contact-form': () => import(/* webpackChunkName: "public-contact" */ './public/contact/index'),
     },
 });
