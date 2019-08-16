31fc5ddc94b2cf012289b634b7dd6779485c3d27
Darryl Hein <dhein@xmmedia.com>
2019-08-15 23:50:16 -0600

    simple security service

---
+++ src/Security/Security.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Security;
+
+use App\Entity\User;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+
+class Security
+{
+    /** @var \Symfony\Component\Security\Core\Security */
+    private $security;
+
+    public function __construct(
+        \Symfony\Component\Security\Core\Security $security
+    ) {
+        $this->security = $security;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->security->getUser();
+    }
+
+    /**
+     * @param mixed $subject
+     */
+    public function isGranted(string $attributes, $subject = null): bool
+    {
+        return $this->security->isGranted($attributes, $subject);
+    }
+
+    public function getToken(): ?TokenInterface
+    {
+        return $this->security->getToken();
+    }
+}
