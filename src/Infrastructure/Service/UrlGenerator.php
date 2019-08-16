240fe90b8c36ea87a06b54fdbfc139d364fb7839
Darryl Hein <dhein@xmmedia.com>
2019-08-15 21:24:44 -0600

    server side url generator

---
+++ src/Infrastructure/Service/UrlGenerator.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Service;
+
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Routing\RouterInterface;
+
+class UrlGenerator
+{
+    /** @var RouterInterface|\Symfony\Bundle\FrameworkBundle\Routing\Router */
+    private $router;
+
+    public function __construct(RouterInterface $router)
+    {
+        $this->router = $router;
+    }
+
+    private function generate(string $name, array $params): string
+    {
+        return $this->router->generate(
+            $name,
+            $params,
+            UrlGeneratorInterface::ABSOLUTE_URL
+        );
+    }
+}
