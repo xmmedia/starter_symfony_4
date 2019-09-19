91483695e97f00b82e3dcd6cbce8dd4088f71cfc
Darryl Hein <dhein@xmmedia.com>
2019-09-19 14:39:09 -0600

    improved gql error logging

--- public/js/src/common/apollo.js
+++ public/js/src/common/apollo.js
@@ -1,14 +1,32 @@
 import Vue from 'vue';
 import { ApolloClient } from 'apollo-client';
+import { ApolloLink } from 'apollo-link';
 import { createHttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import VueApollo from 'vue-apollo';
 import fetch from 'unfetch';
-import { logError } from './lib';
 
 Vue.use(VueApollo);
 
-// HTTP connexion to the API
+// docs: https://www.apollographql.com/docs/react/features/error-handling/
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+    if (graphQLErrors) {
+        graphQLErrors.map((error) => {
+            console.error(
+                `[GraphQL error]: Message: ${error.message}, Location: ${JSON.stringify(error.locations)}, Path: ${error.path}`
+            );
+            if (error.debugMessage) {
+                console.error(error.debugMessage);
+            }
+        });
+    }
+    if (networkError) {
+        console.error(`[Network error]: ${networkError}`);
+    }
+});
+
+// http link
 const httpLink = createHttpLink({
     uri: process.env.REQUEST_CONTEXT_SCHEME+'://'+process.env.REQUEST_CONTEXT_HOST+'/graphql/',
     fetch: fetch,
@@ -16,18 +34,20 @@ const httpLink = createHttpLink({
 
 // Create the apollo client
 const apolloClient = new ApolloClient({
-    link: httpLink,
+    link: ApolloLink.from([errorLink, httpLink]),
     // Cache implementation
     cache: new InMemoryCache(),
     defaultOptions: {
-        // docs: https://www.apollographql.com/docs/react/api/react-apollo/#optionsfetchpolicy
-        fetchPolicy: 'no-cache',
+        query: {
+            // docs: https://www.apollographql.com/docs/react/api/react-apollo/#optionsfetchpolicy
+            fetchPolicy: 'no-cache',
+        },
+        watchQuery: {
+            fetchPolicy: 'no-cache',
+        },
     },
 });
 
 export default new VueApollo({
     defaultClient: apolloClient,
-    errorHandler (e) {
-        logError(e);
-    },
 });
