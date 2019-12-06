bc04b735451a336567bd2fd81d02b90683697d6e
Darryl Hein <dhein@xmmedia.com>
2019-12-06 11:41:23 -0700

    Revert "remove form validation exception event subscriber"
    This reverts commit af4ca68d8b72bd3e990a9d1c1b0be979d24cf3f6.

---
+++ tests/EventSubscriber/FormValidationExceptionSubscriberTest.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\EventSubscriber;
+
+use App\EventSubscriber\FormValidationExceptionSubscriber;
+use App\Tests\BaseTestCase;
+use Exception;
+use Mockery;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\Serializer\SerializerInterface;
+use Xm\SymfonyBundle\Exception\FormValidationException;
+
+class FormValidationExceptionSubscriberTest extends BaseTestCase
+{
+    public function testSubscribedEvents(): void
+    {
+        $subscribed = FormValidationExceptionSubscriber::getSubscribedEvents();
+
+        $this->assertEquals(
+            -100,
+            $subscribed[key($subscribed)][1]
+        );
+    }
+
+    public function testOnKernelExceptionInvalidForm(): void
+    {
+        $subscribed = FormValidationExceptionSubscriber::getSubscribedEvents();
+        $method = $subscribed[key($subscribed)][0];
+
+        $form = Mockery::mock(FormInterface::class);
+        $exception = FormValidationException::fromForm($form);
+
+        $serializer = Mockery::mock(SerializerInterface::class);
+        $serializer->shouldReceive('serialize')
+            ->once()
+            ->with($form, 'json', ['json_encode_options' => JsonResponse::DEFAULT_ENCODING_OPTIONS])
+            ->andReturn('{}');
+
+        $subscriber = new FormValidationExceptionSubscriber($serializer);
+
+        $event = Mockery::mock(GetResponseForExceptionEvent::class);
+
+        $event->shouldReceive('getException')
+            ->once()
+            ->andReturn($exception);
+
+        $event->shouldReceive('setResponse')
+            ->once()
+            ->with(JsonResponse::class);
+
+        $subscriber->{$method}($event);
+    }
+
+    public function testOnKernelExceptionOtherException(): void
+    {
+        $subscribed = FormValidationExceptionSubscriber::getSubscribedEvents();
+        $method = $subscribed[key($subscribed)][0];
+
+        $serializer = Mockery::mock(SerializerInterface::class);
+
+        $subscriber = new FormValidationExceptionSubscriber($serializer);
+
+        $event = Mockery::mock(GetResponseForExceptionEvent::class);
+
+        $exception = new Exception();
+
+        $event->shouldReceive('getException')
+            ->once()
+            ->andReturn($exception);
+
+        $subscriber->{$method}($event);
+    }
+}
