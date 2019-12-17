54860874dfff166e668671f0754f2fe1acc4e95a
Darryl Hein <dhein@xmmedia.com>
2019-12-16 23:22:45 -0700

    disaable save button if saving or saved

--- public/js/src/common/admin_button.vue
+++ public/js/src/common/admin_button.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
-        <button v-if="showButton" type="submit" class="button">
+        <button v-if="showButton"
+                type="submit"
+                class="button"
+                :disabled="isSaving || isSaved">
             <slot>Save</slot>
         </button>
         <slot name="cancel">
@@ -9,11 +12,11 @@
                          class="form-action">Cancel</router-link>
         </slot>
 
-        <span v-if="saving || status === 'saving'" class="ml-4 text-sm italic">
+        <span v-if="isSaving" class="ml-4 text-sm italic">
             <span class="loading" aria-hidden="true" />
             <slot name="saving">Saving...</slot>
         </span>
-        <span v-else-if="saved || status === 'saved'" class="ml-4 text-sm italic">
+        <span v-else-if="isSaved" class="ml-4 text-sm italic">
             <slot name="saved">Saved</slot>
         </span>
     </div>
@@ -41,6 +44,15 @@ export default {
         showButton: {
             type: Boolean,
             default: true,
+        },
+    },
+
+    computed: {
+        isSaving () {
+            return this.saving || this.status === 'saving';
+        },
+        isSaved () {
+            return this.saved || this.status === 'saved';
         },
     },
 };
