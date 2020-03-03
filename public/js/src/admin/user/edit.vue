6e8215da64dd9a16a1f1c37384ab807f5be6509a
Herman Steuernagel <herman@thefourth.ca>
2020-03-03 10:15:00 -0800

    case for Return to List

--- public/js/src/admin/user/edit.vue
+++ public/js/src/admin/user/edit.vue
@@ -2,7 +2,7 @@
     <div class="form-wrap">
         <portal to="header-actions">
             <div class="header-secondary_actions">
-                <router-link :to="{ name: 'admin-user' }">Return to List</router-link>
+                <router-link :to="{ name: 'admin-user' }">Return to list</router-link>
             </div>
         </portal>
 
