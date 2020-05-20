442aa6fba99e3fabaa0f06c88a737fe70145e971
Darryl Hein <dhein@xmmedia.com>
2020-05-20 09:35:35 -0600

    page entity & finder

---
+++ src/Entity/Page.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use App\Model\Page\PageId;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Projection\Page\PageFinder")
+ */
+class Page
+{
+    /**
+     * @var \Ramsey\Uuid\Uuid
+     * @ORM\Id
+     * @ORM\Column(type="uuid")
+     */
+    private $pageId;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=191)
+     */
+    private $path;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=191)
+     */
+    private $title;
+
+    /**
+     * @var array
+     * @ORM\Column(type="json")
+     */
+    private $content;
+
+    /**
+     * @var \DateTimeImmutable
+     * @ORM\Column(type="datetime_immutable")
+     */
+    private $lastModified;
+
+    /**
+     * @var User|null
+     * @ORM\ManyToOne(targetEntity="App\Entity\User")
+     * @ORM\JoinColumn(referencedColumnName="user_id", nullable=true)
+     */
+    private $lastModifiedBy;
+
+    public function pageId(): PageId
+    {
+        return PageId::fromUuid($this->pageId);
+    }
+
+    public function path(): string
+    {
+        return $this->path;
+    }
+
+    public function title(): string
+    {
+        return $this->title;
+    }
+
+    public function content(): array
+    {
+        return $this->content;
+    }
+
+    public function lastModified(): \DateTimeImmutable
+    {
+        return $this->lastModified;
+    }
+
+    public function lastModifiedBy(): ?User
+    {
+        return $this->lastModifiedBy;
+    }
+}
