722594cf25e77335ca6161ec3bb79420b8f76323
Darryl Hein <dhein@xmmedia.com>
2020-08-05 23:07:36 -0600

    remove references to fixtures

--- tests/WebTestCase.php
+++ tests/WebTestCase.php
@@ -4,36 +4,14 @@ declare(strict_types=1);
 
 namespace App\Tests;
 
-use App\DataFixtures\ORM\LoadDefaultFixtures;
 use Liip\FunctionalTestBundle\Test\WebTestCase as BaseWebTestCase;
 use Symfony\Bundle\FrameworkBundle\Client;
 
 class WebTestCase extends BaseWebTestCase
 {
-    /**
-     * List of fixtures to load.
-     *
-     * @var array
-     */
-    protected $fixtureList = [
-        LoadDefaultFixtures::class,
-    ];
-
-    /**
-     * @var \Doctrine\Common\DataFixtures\ReferenceRepository
-     */
-    protected $fixtures;
-
     protected function setUp(): void
     {
         parent::setUp();
-    }
-
-    protected function logIn($userRef = 'user-regular')
-    {
-        $user = $this->fixtures->getReference($userRef);
-
-        $this->loginAs($user, 'main');
     }
 
     protected function assertPathInfoMatches(Client $client, $regExp)
