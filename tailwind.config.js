75dcb37bac58f2fac78480d3bd7cbff623536fa6
Darryl Hein <dhein@xmmedia.com>
2020-08-21 14:17:05 -0600

    enable new tailwind features

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,18 @@ module.exports = {
     future: {
         removeDeprecatedGapUtilities: true,
     },
+    experimental: {
+        // https://github.com/tailwindlabs/tailwindcss/releases/tag/v1.7.0#use-apply-with-variants-and-other-complex-classes
+        applyComplexClasses: true,
+        // https://github.com/tailwindlabs/tailwindcss/releases/tag/v1.7.0#new-color-palette
+        uniformColorPalette: true,
+        // https://github.com/tailwindlabs/tailwindcss/releases/tag/v1.7.0#extended-spacing-scale
+        extendedSpacingScale: true,
+        // https://github.com/tailwindlabs/tailwindcss/releases/tag/v1.7.0#default-line-heights-per-font-size-by-default
+        defaultLineHeights: true,
+        // https://github.com/tailwindlabs/tailwindcss/releases/tag/v1.7.0#extended-font-size-scale
+        extendedFontSizeScale: true,
+    },
     purge: {
         content: [
             './templates/**/*.html.twig',
