e3495bf160cb4e02783e90c31162b61eab5575bf
Darryl Hein <dhein@xmmedia.com>
2020-10-26 10:50:57 -0600

    force composer v1

--- setup_dev.sh
+++ setup_dev.sh
@@ -80,10 +80,7 @@ printf "\n\n"
 cd $RELEASE
 
 echo "Install Composer"
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === 'e5325b19b381bfd88ce90a5ddb7823406b2a38cff6bb704b0acc289a09c8128d4a8ce2bbafcd1fcbdc38666422fe2806') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php
-php -r "unlink('composer-setup.php');"
+wget --no-verbose -O composer.phar https://getcomposer.org/composer-1.phar
 php composer.phar selfupdate
 printf "\n\n"
 
