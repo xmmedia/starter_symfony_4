e9890ac33b86c2e6a6fd9664e518f3a749fa459c
Darryl Hein <dhein@xmmedia.com>
2020-11-17 14:25:06 -0700

    use projection with args trait in user proj. test

--- tests/Projection/User/UserProjectionTest.php
+++ tests/Projection/User/UserProjectionTest.php
@@ -9,9 +9,12 @@ use App\Projection\User\UserProjection;
 use App\Tests\BaseTestCase;
 use Mockery;
 use Prooph\EventStore\Projection\ReadModelProjector;
+use Xm\SymfonyBundle\Tests\ProjectionWhenArgs;
 
 class UserProjectionTest extends BaseTestCase
 {
+    use ProjectionWhenArgs;
+
     public function test(): void
     {
         $projectedEvents = [
@@ -37,26 +40,7 @@ class UserProjectionTest extends BaseTestCase
             ->andReturnSelf();
 
         $projector->shouldReceive('when')
-            ->withArgs(function ($eventHandlers) use ($projectedEvents) {
-                if (!\is_array($eventHandlers)) {
-                    return false;
-                }
-
-                foreach ($projectedEvents as $event) {
-                    if (!\array_key_exists($event, $eventHandlers)) {
-                        return false;
-                    }
-                }
-
-                // make sure there are not extra events
-                foreach ($eventHandlers as $event => $handler) {
-                    if (false === array_search($event, $projectedEvents)) {
-                        return false;
-                    }
-                }
-
-                return true;
-            });
+            ->withArgs($this->whenArgs($projectedEvents));
 
         $projection->project($projector);
     }
