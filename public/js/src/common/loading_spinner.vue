8c0b521da0b020c97245411022ac4d53d3c063f1
Darryl Hein <dhein@xmmedia.com>
2020-12-09 16:45:59 -0700

    allow customizing spinner classes

--- public/js/src/common/loading_spinner.vue
+++ public/js/src/common/loading_spinner.vue
@@ -1,6 +1,18 @@
 <template>
     <div class="flex justify-center items-center">
         <div class="loading" aria-hidden="true" />
+        <div class="loading" :class="spinnerClasses" aria-hidden="true" />
         <slot></slot>
     </div>
 </template>
+
+<script>
+export default {
+    props: {
+        spinnerClasses: {
+            type: String,
+            default: null,
+        },
+    },
+}
+</script>
