eadd7f9146222f4bad48a77a42d4af71eab01677
Darryl Hein <dhein@xmmedia.com>
2020-12-22 16:21:12 -0700

    switch gql Role to class
    so it updates as roles are updated

---
+++ src/Infrastructure/GraphQl/Type/RoleType.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\GraphQl\Type;
+
+use App\Model\User\Role;
+use GraphQL\Error\Error;
+use GraphQL\Language\AST\EnumValueNode;
+use GraphQL\Type\Definition\EnumType;
+use GraphQL\Utils\Utils;
+use Overblog\GraphQLBundle\Definition\Resolver\AliasedInterface;
+
+class RoleType extends EnumType implements AliasedInterface
+{
+    private const NAME = 'Role';
+
+    public function __construct()
+    {
+        $config = [
+            'name'        => self::NAME,
+            'values'      => array_combine(
+                Role::getNames(),
+                Role::getValues()
+            ),
+            'description' => 'Available User roles.',
+        ];
+
+        parent::__construct($config);
+    }
+
+    /**
+     * @param Role|string $value
+     *
+     * @return string
+     */
+    public function serialize($value): ?string
+    {
+        if (null === $value) {
+            return null;
+        }
+
+        if (is_string($value)) {
+            $value = Role::byValue($value);
+        }
+
+        if ($value instanceof Role) {
+            return $value->getValue();
+        }
+
+        throw new Error('Cannot serialize Role value as enum: '.Utils::printSafe($value));
+    }
+
+    /**
+     * @param string $value
+     */
+    public function parseValue($value): ?Role
+    {
+        if (null === $value) {
+            return null;
+        }
+
+        return Role::byValue($value);
+    }
+
+    /**
+     * @param EnumValueNode $valueNode
+     */
+    public function parseLiteral($valueNode, array $variables = null): ?Role
+    {
+        if (!$valueNode instanceof EnumValueNode) {
+            return null;
+        }
+
+        return $this->parseValue($valueNode->value);
+    }
+
+    public static function getAliases(): array
+    {
+        return [self::NAME];
+    }
+}
---
+++ tests/Infrastructure/GraphQl/Type/RoleTypeTest.php
@@ -0,0 +1,87 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Infrastructure\GraphQl\Type;
+
+use App\Infrastructure\GraphQl\Type\RoleType;
+use App\Model\User\Role;
+use GraphQL\Language\AST\EnumValueNode;
+use GraphQL\Language\AST\StringValueNode;
+use GraphQL\Language\AST\VariableNode;
+use PHPUnit\Framework\TestCase;
+
+class RoleTypeTest extends TestCase
+{
+    /**
+     * @dataProvider dateProvider
+     */
+    public function testSerialize($value, ?string $expected): void
+    {
+        $result = (new RoleType())->serialize($value);
+
+        $this->assertEquals($expected, $result);
+    }
+
+    /**
+     * @dataProvider dateProvider
+     */
+    public function testParseValue($expected, ?string $value): void
+    {
+        $result = (new RoleType())->parseValue($value);
+
+        $this->assertEquals($expected, $result);
+    }
+
+    /**
+     * @dataProvider dateProvider
+     */
+    public function testParseLiteral($expected, ?string $value): void
+    {
+        $valueNode = new EnumValueNode([]);
+        $valueNode->value = $value;
+
+        $result = (new RoleType())->parseLiteral($valueNode);
+
+        $this->assertEquals($expected, $result);
+    }
+
+    public function dateProvider(): \Generator
+    {
+        yield [
+            Role::ROLE_USER(),
+            'ROLE_USER',
+        ];
+
+        yield [
+            'ROLE_USER',
+            'ROLE_USER',
+        ];
+
+        yield [
+            null,
+            null,
+        ];
+    }
+
+    public function testParseValueInvalid(): void
+    {
+        $this->expectException(\Exception::class);
+
+        (new RoleType())->parseValue('asdf');
+    }
+
+    public function testParseLiteralNotStringValueNode(): void
+    {
+        $result = (new RoleType())->parseLiteral(new EnumValueNode([]));
+
+        $this->assertNull($result);
+    }
+
+    public function testAliases(): void
+    {
+        $result = RoleType::getAliases();
+
+        $this->assertEquals(['Role'], $result);
+    }
+}
