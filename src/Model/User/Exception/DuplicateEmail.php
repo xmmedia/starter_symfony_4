761847e00e5889fe5edcb90d5a40c2d0e05f8e50
Darryl Hein <dhein@xmmedia.com>
2020-12-22 20:42:37 -0700

    change param types on expection

--- src/Model/User/Exception/DuplicateEmail.php
+++ src/Model/User/Exception/DuplicateEmail.php
@@ -5,17 +5,18 @@ declare(strict_types=1);
 namespace App\Model\User\Exception;
 
 use App\Model\User\UserId;
+use App\Model\User\UserIdInterface;
 use Xm\SymfonyBundle\Model\Email;
 
 final class DuplicateEmail extends \InvalidArgumentException
 {
-    public static function withEmail(Email $email, UserId $userId): self
+    public static function withEmail(Email $email, UserIdInterface $userId): self
     {
         return new self(
             sprintf(
                 'The email address "%s" is already used by user "%s".',
                 $email,
-                $userId
+                $userId,
             )
         );
     }
