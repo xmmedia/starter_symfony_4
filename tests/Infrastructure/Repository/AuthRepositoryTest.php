dc0b8abd0a126b5f127782d99e768670f5161372
Darryl Hein <dhein@xmmedia.com>
2020-12-30 14:27:33 -0700

    basic ar repo tests

---
+++ tests/Infrastructure/Repository/AuthRepositoryTest.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Infrastructure\Repository;
+
+use App\Infrastructure\Repository\AuthRepository;
+use App\Model\Auth\Auth;
+use App\Tests\BaseTestCase;
+use Xm\SymfonyBundle\Util\Tests\AggregateRepositoryFactory;
+
+class AuthRepositoryTest extends BaseTestCase
+{
+    use AggregateRepositoryFactory;
+
+    public function testSaveGet(): void
+    {
+        $faker = $this->faker();
+
+        $auth = Auth::success(
+            $faker->authId,
+            $faker->userId,
+            $faker->emailVo,
+            $faker->userAgent,
+            $faker->ipv4,
+        );
+
+        $repository = $this->getRepository(AuthRepository::class, Auth::class);
+
+        $repository->save($auth);
+
+        $fetchedAuth = $repository->get($auth->authId());
+
+        $this->assertInstanceOf(Auth::class, $fetchedAuth);
+        $this->assertNotSame($auth, $fetchedAuth);
+        $this->assertSameValueAs($auth->authId(), $fetchedAuth->authId());
+    }
+
+    public function testGetDoesntExist(): void
+    {
+        $faker = $this->faker();
+
+        $fetchedAuth = $this->getRepository(AuthRepository::class, Auth::class)
+            ->get($faker->authId);
+
+        $this->assertNull($fetchedAuth);
+    }
+}
---
+++ tests/Infrastructure/Repository/UserRepositoryTest.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Infrastructure\Repository;
+
+use App\Infrastructure\Repository\UserRepository;
+use App\Model\User\Role;
+use App\Model\User\Service\ChecksUniqueUsersEmail;
+use App\Model\User\User;
+use App\Tests\BaseTestCase;
+use Mockery;
+use Xm\SymfonyBundle\Util\Tests\AggregateRepositoryFactory;
+
+class UserRepositoryTest extends BaseTestCase
+{
+    use AggregateRepositoryFactory;
+
+    public function testSaveGet(): void
+    {
+        $faker = $this->faker();
+
+        $checksUniqueUsersEmail = Mockery::mock(ChecksUniqueUsersEmail::class);
+        $checksUniqueUsersEmail->shouldReceive('__invoke')
+            ->andReturnNull();
+
+        $user = User::addByAdminMinimum(
+            $faker->userId,
+            $faker->emailVo,
+            $faker->password,
+            Role::ROLE_USER(),
+            $checksUniqueUsersEmail,
+        );
+
+        $repository = $this->getRepository(UserRepository::class, User::class);
+
+        $repository->save($user);
+
+        $fetchedUser = $repository->get($user->userId());
+
+        $this->assertInstanceOf(User::class, $fetchedUser);
+        $this->assertNotSame($user, $fetchedUser);
+        $this->assertSameValueAs($user->userId(), $fetchedUser->userId());
+    }
+
+    public function testGetDoesntExist(): void
+    {
+        $faker = $this->faker();
+
+        $fetchedUser = $this->getRepository(UserRepository::class, User::class)
+            ->get($faker->userId);
+
+        $this->assertNull($fetchedUser);
+    }
+}
