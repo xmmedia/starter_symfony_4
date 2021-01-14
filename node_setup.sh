6f28348c10f554ace31ea35dc7078cce163d039e
Darryl Hein <dhein@xmmedia.com>
2021-01-13 21:53:24 -0700

    update the default node version

--- node_setup.sh
+++ node_setup.sh
@@ -2,7 +2,7 @@
 
 echo "-- Setting up node"
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
 
 nvm install
 nvm use --delete-prefix --silent
