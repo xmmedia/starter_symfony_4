3ceb2d21a97cc7904f2e7e99108f33c72fbb4f6b
Darryl Hein <dhein@xmmedia.com>
2022-01-25 13:37:29 -0700

    tailwind 3

--- public/js/src/admin/user_profile_edit/component/tabs.vue
+++ public/js/src/admin/user_profile_edit/component/tabs.vue
@@ -1,13 +1,13 @@
 <template>
     <ul class="list-none flex justify-between pl-0">
-        <li class="flex-grow">
+        <li class="grow">
             <router-link :to="{ name: 'user-profile-edit' }"
                          :class="linkActiveClass('user-profile-edit')"
                          class="block p-4 text-center hover:no-underline rounded-tl-sm">
                 Profile
             </router-link>
         </li>
-        <li class="flex-grow">
+        <li class="grow">
             <router-link :to="{ name: 'user-profile-edit-password' }"
                          :class="linkActiveClass('user-profile-edit-password')"
                          class="block p-4 text-center hover:no-underline rounded-tr-sm">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,56 +1,26 @@
 const plugin = require('tailwindcss/plugin');
-const colors = require('tailwindcss/colors');
 
 module.exports = {
     mode: 'jit',
-    purge: {
-        content: [
-            './templates/**/*.html.twig',
-            './public/js/src/**/*.vue',
-            './public/js/src/**/*.js',
-        ],
-        options: {
-            safelist: [
-                // vue transition classes: https://vuejs.org/v2/guide/transitions.html#Transition-Classes
-                '.md-enter-active',
-                '.md-leave-active',
-                '.md-enter',
-                '.md-leave-active',
-            ],
-        },
-    },
+    content: [
+        './templates/**/*.html.twig',
+        './public/js/src/**/*.vue',
+        './public/js/src/**/*.js',
+    ],
+    safelist: [
+        // vue transition classes: https://vuejs.org/v2/guide/transitions.html#Transition-Classes
+        '.md-enter-active',
+        '.md-leave-active',
+        '.md-enter',
+        '.md-leave-active',
+    ],
     theme: {
-        colors: {
-            transparent: 'transparent',
-            current: 'currentColor',
-            inherit: 'inherit',
-
-            black: colors.black,
-            white: colors.white,
-            rose: colors.rose,
-            pink: colors.pink,
-            fuchsia: colors.fuchsia,
-            purple: colors.purple,
-            violet: colors.violet,
-            indigo: colors.indigo,
-            blue: colors.blue,
-            sky: colors.sky,
-            cyan: colors.cyan,
-            teal: colors.teal,
-            emerald: colors.emerald,
-            green: colors.green,
-            lime: colors.lime,
-            yellow: colors.yellow,
-            amber: colors.amber,
-            orange: colors.orange,
-            red: colors.red,
-            warmGray: colors.warmGray,
-            trueGray: colors.trueGray,
-            gray: colors.gray,
-            coolGray: colors.coolGray,
-            blueGray: colors.blueGray,
-        },
         extend: {
+            colors: {
+                transparent: 'transparent',
+                current: 'currentColor',
+                inherit: 'inherit',
+            },
             borderWidth: {
                 '10': '10px',
             },
@@ -77,14 +47,6 @@ module.exports = {
                 ],
             },
         },
-    },
-    variants: {
-        borderColor: ['responsive', 'hover', 'focus', 'group-hover'],
-        cursor: ['responsive', 'disabled'],
-        margin: ['responsive', 'focus'],
-        opacity: ['responsive', 'hover', 'focus', 'group-hover'],
-        padding: ['responsive', 'focus'],
-        textColor: ['responsive', 'hover', 'focus', 'group-hover'],
     },
     plugins: [
         require('@tailwindcss/typography'),
