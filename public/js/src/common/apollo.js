803942c97b07ad68bda0cebdea9eb982b8fb9522
Darryl Hein <dhein@xmmedia.com>
2022-01-25 12:26:07 -0700

    just using the window location for gql uri

--- public/js/src/common/apollo.js
+++ public/js/src/common/apollo.js
@@ -34,7 +34,7 @@ const errorLink = onError(({ graphQLErrors, networkError }) => {
 
 // http link
 const httpLink = createHttpLink({
-    uri: process.env.REQUEST_CONTEXT_SCHEME+'://'+process.env.REQUEST_CONTEXT_HOST+'/graphql',
+    uri: window.location.origin+'/graphql',
     fetch,
 });
 
