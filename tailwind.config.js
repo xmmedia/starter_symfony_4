4cc9c5bd7e1034d8af7000fbc50b1c203290bf75
Darryl Hein <dhein@xmmedia.com>
2022-01-25 21:32:04 -0700

    remove colours included with tailwind
    leave key so clear how to add custom colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,9 +17,6 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                transparent: 'transparent',
-                current: 'currentColor',
-                inherit: 'inherit',
             },
             borderWidth: {
                 '10': '10px',
