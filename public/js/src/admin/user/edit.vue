409fe744be0b4dce0a4106cb5d3fbcbf7d5739db
Darryl Hein <dhein@xmmedia.com>
2022-08-04 17:22:03 -0600

    correct when save is allowed

--- public/js/src/admin/user/edit.vue
+++ public/js/src/admin/user/edit.vue
@@ -164,7 +164,7 @@ export default {
                 return false;
             }
 
-            return !this.state.matches('ready.saving') && !this.state.matches('ready.saved');
+            return this.state.matches('ready.ready');
         },
     },
 
