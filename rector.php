6b7f5e2fce32f32dd18b658d164e4c88d4f6b5ff
Darryl Hein <dhein@xmmedia.com>
2023-01-09 15:13:22 -0700

    fix use of rector config

--- rector.php
+++ rector.php
@@ -3,30 +3,27 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Core\Configuration\Option;
 use Rector\Doctrine\Set\DoctrineSetList;
 use Rector\Php74\Rector\Property\TypedPropertyRector;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Symfony\Set\SymfonySetList;
 
-return static function (RectorConfig $containerConfigurator): void {
-    // get parameters
-    $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::PATHS, [
-        __DIR__.'/src',
+return static function (RectorConfig $config): void {
+    $config->paths([
+        __DIR__ . '/src',
     ]);
 
     // Define what rule sets will be applied
-    $containerConfigurator->import(LevelSetList::UP_TO_PHP_80);
-    $containerConfigurator->import(DoctrineSetList::DOCTRINE_CODE_QUALITY);
-    $containerConfigurator->import(DoctrineSetList::ANNOTATIONS_TO_ATTRIBUTES);
-    $containerConfigurator->import(SymfonySetList::SYMFONY_54);
-    $containerConfigurator->import(SymfonySetList::SYMFONY_CODE_QUALITY);
-    $containerConfigurator->import(SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION);
-    $containerConfigurator->import(SymfonySetList::ANNOTATIONS_TO_ATTRIBUTES);
+    $config->import(LevelSetList::UP_TO_PHP_80);
+    $config->import(DoctrineSetList::DOCTRINE_CODE_QUALITY);
+    $config->import(DoctrineSetList::ANNOTATIONS_TO_ATTRIBUTES);
+    $config->import(SymfonySetList::SYMFONY_54);
+    $config->import(SymfonySetList::SYMFONY_CODE_QUALITY);
+    $config->import(SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION);
+    $config->import(SymfonySetList::ANNOTATIONS_TO_ATTRIBUTES);
 
     // get services (needed for register a single rule)
-    $services = $containerConfigurator->services();
+    $services = $config->services();
 
     // register a single rule
     $services->set(TypedPropertyRector::class);
