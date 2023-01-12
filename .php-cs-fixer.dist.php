b9c8856fd0ffae226a3bb322ed7585754cfe620d
Darryl Hein <dhein@xmmedia.com>
2023-01-12 11:10:34 -0700

    cs

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,46 +1,46 @@
 <?php
 
+declare(strict_types=1);
+
 // Great tool for configuration: https://mlocati.github.io/php-cs-fixer-configurator/
 
 $finder = PhpCsFixer\Finder::create()
     ->in(__DIR__)
     ->exclude('var')
-    ->exclude('node_modules')
-;
+    ->exclude('node_modules');
 
 return (new PhpCsFixer\Config())
     ->setRiskyAllowed(true)
     ->setRules([
-        '@Symfony' => true,
-        '@Symfony:risky' => true,
-        '@PSR2' => true,
-        '@DoctrineAnnotation' => true,
-        '@PHP71Migration' => true,
-        '@PHP73Migration' => true,
-        '@PHP74Migration' => true,
-        '@PHP80Migration' => true,
-        '@PHP81Migration' => true,
-        '@PHPUnit60Migration:risky' => true,
-        'array_syntax' => [
-            'syntax' => 'short',
+        '@Symfony'                    => true,
+        '@Symfony:risky'              => true,
+        '@PSR2'                       => true,
+        '@DoctrineAnnotation'         => true,
+        '@PHP71Migration'             => true,
+        '@PHP73Migration'             => true,
+        '@PHP74Migration'             => true,
+        '@PHP80Migration'             => true,
+        '@PHP81Migration'             => true,
+        '@PHPUnit60Migration:risky'   => true,
+        'array_syntax'                => [
+        'syntax' => 'short',
         ],
-        'binary_operator_spaces' => [
-            'operators' => [
-                '=>' => 'align',
-            ],
+        'binary_operator_spaces'      => [
+        'operators' => [
+        '=>' => 'align',
         ],
-        'declare_strict_types' => true,
-        'fopen_flags' => false,
-        'heredoc_indentation' => false,
-        'method_argument_space' => [
-            'on_multiline' => 'ensure_fully_multiline',
         ],
-        'ordered_imports' => true,
-        'protected_to_private' => false,
+        'declare_strict_types'        => true,
+        'fopen_flags'                 => false,
+        'heredoc_indentation'         => false,
+        'method_argument_space'       => [
+        'on_multiline' => 'ensure_fully_multiline',
+        ],
+        'ordered_imports'             => true,
+        'protected_to_private'        => false,
         'trailing_comma_in_multiline' => [
-            'elements' => ['arrays', 'arguments', 'parameters'],
+        'elements' => ['arrays', 'arguments', 'parameters'],
         ],
-        'single_line_throw' => false,
+        'single_line_throw'           => false,
     ])
-    ->setFinder($finder)
-;
+    ->setFinder($finder);
