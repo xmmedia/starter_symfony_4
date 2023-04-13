61245332cb5e8add91a54deff7b5c0c8be982247
Darryl Hein <dhein@xmmedia.com>
2023-04-13 14:19:23 -0600

    new vue store create

--- public/js/src/admin/store.js
+++ public/js/src/admin/store.js
@@ -1,11 +1,8 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
+import { createStore } from 'vuex';
 
 import adminMenu from './menu/store';
 
-Vue.use(Vuex);
-
-export default new Vuex.Store({
+export default createStore({
     namespaced: true,
     strict: process.env.NODE_ENV !== 'production',
 
