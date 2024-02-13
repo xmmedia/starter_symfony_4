01360c2b1ecb5e8d19a5e519838b351990d81951
Darryl Hein <dhein@xmmedia.com>
2024-02-12 20:03:25 -0700

    add phone & email view components

---
+++ public/js/src/common/view_email.vue
@@ -0,0 +1,12 @@
+<template>
+    <a :href="'mailto:'+email">{{ email }}</a>
+</template>
+
+<script setup>
+defineProps({
+    email: {
+        type: String,
+        required: true,
+    },
+});
+</script>
---
+++ public/js/src/common/view_phone.vue
@@ -0,0 +1,16 @@
+<template>
+    <a :href="formatPhone(phoneNumber, 'RFC3966')">
+        {{ formatPhone(phoneNumber) }}
+    </a>
+</template>
+
+<script setup>
+import { formatPhone } from '@/common/lib';
+
+defineProps({
+    phoneNumber: {
+        type: [Object, String],
+        required: true,
+    },
+});
+</script>
