8d01af1d8356c4274d1b7bdfca98d6d7ff1ad026
Darryl Hein <dhein@xmmedia.com>
2024-02-12 21:59:15 -0700

    rector config updates

--- rector.php
+++ rector.php
@@ -19,20 +19,18 @@ return static function (RectorConfig $config): void {
     // Symfony rules: https://github.com/rectorphp/rector-symfony/blob/main/docs/rector_rules_overview.md
     $config->import(DoctrineSetList::DOCTRINE_CODE_QUALITY);
     $config->import(DoctrineSetList::ANNOTATIONS_TO_ATTRIBUTES);
-    $config->import(SymfonySetList::SYMFONY_62);
+    $config->import(SymfonySetList::SYMFONY_63);
     $config->import(SymfonySetList::SYMFONY_CODE_QUALITY);
     $config->import(SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION);
     $config->import(SymfonySetList::ANNOTATIONS_TO_ATTRIBUTES);
     $config->rule(RectorRules\ArrowFunction\AddArrowFunctionReturnTypeRector::class);
-    $config->rule(RectorRules\Closure\AddClosureReturnTypeRector::class);
+    $config->rule(RectorRules\ArrowFunction\AddArrowFunctionReturnTypeRector::class);
     $config->rule(RectorRules\ClassMethod\AddMethodCallBasedStrictParamTypeRector::class);
     $config->rule(RectorRules\ClassMethod\AddParamTypeBasedOnPHPUnitDataProviderRector::class);
     $config->rule(RectorRules\ClassMethod\AddParamTypeFromPropertyTypeRector::class);
     $config->rule(RectorRules\ClassMethod\AddReturnTypeDeclarationBasedOnParentClassMethodRector::class);
     $config->rule(RectorRules\FunctionLike\AddReturnTypeDeclarationFromYieldsRector::class);
-    $config->ruleWithConfiguration(RectorRules\ClassMethod\AddVoidReturnTypeWhereNoReturnRector::class, [
-        RectorRules\ClassMethod\AddVoidReturnTypeWhereNoReturnRector::USE_PHPDOC => true,
-    ]);
+    $config->rule(RectorRules\ClassMethod\AddVoidReturnTypeWhereNoReturnRector::class);
     $config->rule(RectorRules\ClassMethod\ParamTypeByMethodCallTypeRector::class);
     $config->rule(RectorRules\ClassMethod\ParamTypeByParentCallTypeRector::class);
     $config->rule(RectorRules\Param\ParamTypeFromStrictTypedPropertyRector::class);
@@ -51,5 +49,6 @@ return static function (RectorConfig $config): void {
     $config->rule(RectorRules\Property\TypedPropertyFromStrictSetUpRector::class);
 
     // get services (needed for register a single rule)
-    $config->services();
+    // $config->singleton(ServiceType::class)
+    // $config->services();
 };
