d5d8ac3ae8e75398842176cf0c986abc1e3b0b85
Darryl Hein <dhein@xmmedia.com>
2024-02-14 21:52:13 -0700

    updated rector config

--- rector.php
+++ rector.php
@@ -3,52 +3,47 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Doctrine\Set\DoctrineSetList;
-use Rector\Symfony\Set\SymfonySetList;
 use Rector\TypeDeclaration\Rector as RectorRules;
 
-return static function (RectorConfig $config): void {
-    $config->paths([
+return RectorConfig::configure()
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        codingStyle: true,
+        typeDeclarations: true,
+        strictBooleans: true,
+        // no privatization, naming, instanceOf, and earlyReturn
+    )
+    ->withAttributesSets(symfony: true, doctrine: true)
+    ->withPhpSets()
+    ->withTypeCoverageLevel(29)
+    ->withDeadCodeLevel(40)
+    ->withPaths([
         __DIR__.'/src',
         __DIR__.'/tests',
-    ]);
-
-    // Define what rule sets will be applied
-    // List of built-in Rector rules: https://github.com/rectorphp/rector/blob/main/docs/rector_rules_overview.md
-    // Doctrine rules: https://github.com/rectorphp/rector-doctrine/blob/main/docs/rector_rules_overview.md
-    // Symfony rules: https://github.com/rectorphp/rector-symfony/blob/main/docs/rector_rules_overview.md
-    $config->import(DoctrineSetList::DOCTRINE_CODE_QUALITY);
-    $config->import(DoctrineSetList::ANNOTATIONS_TO_ATTRIBUTES);
-    $config->import(SymfonySetList::SYMFONY_63);
-    $config->import(SymfonySetList::SYMFONY_CODE_QUALITY);
-    $config->import(SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION);
-    $config->import(SymfonySetList::ANNOTATIONS_TO_ATTRIBUTES);
-    $config->rule(RectorRules\ArrowFunction\AddArrowFunctionReturnTypeRector::class);
-    $config->rule(RectorRules\ArrowFunction\AddArrowFunctionReturnTypeRector::class);
-    $config->rule(RectorRules\ClassMethod\AddMethodCallBasedStrictParamTypeRector::class);
-    $config->rule(RectorRules\ClassMethod\AddParamTypeBasedOnPHPUnitDataProviderRector::class);
-    $config->rule(RectorRules\ClassMethod\AddParamTypeFromPropertyTypeRector::class);
-    $config->rule(RectorRules\ClassMethod\AddReturnTypeDeclarationBasedOnParentClassMethodRector::class);
-    $config->rule(RectorRules\FunctionLike\AddReturnTypeDeclarationFromYieldsRector::class);
-    $config->rule(RectorRules\ClassMethod\AddVoidReturnTypeWhereNoReturnRector::class);
-    $config->rule(RectorRules\ClassMethod\ParamTypeByMethodCallTypeRector::class);
-    $config->rule(RectorRules\ClassMethod\ParamTypeByParentCallTypeRector::class);
-    $config->rule(RectorRules\Param\ParamTypeFromStrictTypedPropertyRector::class);
-    $config->rule(RectorRules\ClassMethod\ReturnTypeFromReturnDirectArrayRector::class);
-    $config->rule(RectorRules\ClassMethod\ReturnTypeFromReturnNewRector::class);
-    $config->rule(RectorRules\ClassMethod\ReturnTypeFromStrictBoolReturnExprRector::class);
-    $config->rule(RectorRules\ClassMethod\ReturnTypeFromStrictConstantReturnRector::class);
-    $config->rule(RectorRules\ClassMethod\ReturnTypeFromStrictNativeCallRector::class);
-    $config->rule(RectorRules\ClassMethod\ReturnTypeFromStrictNewArrayRector::class);
-    $config->rule(RectorRules\ClassMethod\ReturnTypeFromStrictTypedCallRector::class);
-    $config->ruleWithConfiguration(RectorRules\Property\TypedPropertyFromAssignsRector::class, [
-        RectorRules\Property\TypedPropertyFromAssignsRector::INLINE_PUBLIC => true,
-    ]);
-    $config->rule(RectorRules\Property\TypedPropertyFromStrictConstructorRector::class);
-    $config->rule(RectorRules\Property\TypedPropertyFromStrictGetterMethodReturnTypeRector::class);
-    $config->rule(RectorRules\Property\TypedPropertyFromStrictSetUpRector::class);
-
-    // get services (needed for register a single rule)
-    // $config->singleton(ServiceType::class)
-    // $config->services();
-};
+    ])
+    ->withRootFiles()
+    ->withSkip([
+        // don't remove useless variables inside AR events
+        // it's nice to keep them for editing later
+        \Rector\CodeQuality\Rector\FunctionLike\SimplifyUselessVariableRector::class => [
+            __DIR__.'/src/Model/*/Event/*',
+        ],
+        // we may not want the property to have a default value
+        \Rector\Php74\Rector\Property\RestoreDefaultNullToNullableTypePropertyRector::class,
+        \Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector::class,
+        // from set "codingStyle"
+        \Rector\CodingStyle\Rector\Stmt\NewlineAfterStatementRector::class,
+        \Rector\CodingStyle\Rector\If_\NullableCompareToNullRector::class,
+        \Rector\CodingStyle\Rector\Catch_\CatchExceptionNameMatchingTypeRector::class,
+        \Rector\CodingStyle\Rector\Assign\SplitDoubleAssignRector::class,
+        \Rector\CodingStyle\Rector\String_\SymplifyQuoteEscapeRector::class,
+        // from set "strictBooleans"
+        \Rector\Strict\Rector\If_\BooleanInIfConditionRuleFixerRector::class,
+        \Rector\Strict\Rector\BooleanNot\BooleanInBooleanNotRuleFixerRector::class,
+        // from set "codingStyle"
+        \Rector\CodeQuality\Rector\Identical\FlipTypeControlToUseExclusiveTypeRector::class,
+        \Rector\CodeQuality\Rector\If_\CombineIfRector::class,
+        \Rector\CodeQuality\Rector\If_\ExplicitBoolCompareRector::class,
+    ])
+;
