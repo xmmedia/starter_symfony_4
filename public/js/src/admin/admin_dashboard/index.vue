b4173d1119fe2dd9955e39689122c0c0355a4b7c
Darryl Hein <dhein@xmmedia.com>
2024-02-29 16:18:55 -0700

    cs

--- public/js/src/admin/admin_dashboard/index.vue
+++ public/js/src/admin/admin_dashboard/index.vue
@@ -5,9 +5,7 @@
     <ul class="flex flex-wrap pl-0 list-none">
         <li>
             <RouterLink :to="{ name: 'admin-user' }" class="dashboard-link">
-                <AdminIcon icon="users"
-                           width="96"
-                           height="96" />
+                <AdminIcon icon="users" width="96" height="96" />
                 Users
             </RouterLink>
         </li>
