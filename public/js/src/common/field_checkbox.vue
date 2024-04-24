46d7bac3bc7199ebbd540a8739436f53a1102ab6
Darryl Hein <dhein@xmmedia.com>
2024-04-24 12:17:11 -0600

    add checkbox field component

---
+++ public/js/src/common/field_checkbox.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="field-wrap-checkbox">
+        <FieldError v-if="v" :v="v">
+            <template #required><slot name="required"></slot></template>
+        </FieldError>
+
+        <input :id="id" v-model="checked" type="checkbox" :value="true">
+        <label :for="id"><slot></slot></label>
+
+        <div v-if="!!$slots.help" class="field-help">
+            <slot name="help"></slot>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import cuid from 'cuid';
+import { computed } from 'vue';
+
+const emit = defineEmits([ 'update:modelValue' ]);
+
+const props = defineProps({
+    modelValue: {
+        type: Boolean,
+        default: null,
+    },
+    v: {
+        type: Object,
+        default: null,
+    },
+    id: {
+        type: String,
+        default: function () {
+            return cuid();
+        },
+    },
+});
+
+const checked = computed({
+    get () {
+        return props.modelValue;
+    },
+    set (value) {
+        emit('update:modelValue', value);
+    },
+});
+</script>
