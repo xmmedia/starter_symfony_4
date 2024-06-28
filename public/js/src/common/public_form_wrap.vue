5db69c0a12c90ab7e137d12a40491e7b3f1862dc
Darryl Hein <dhein@xmmedia.com>
2024-06-28 09:17:17 -0600

    remove classes directly into html

--- public/js/src/common/public_form_wrap.vue
+++ public/js/src/common/public_form_wrap.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="form-wrap my-8 pb-10">
-        <div class="form_header-wrap">
-            <h1 class="form-header"><slot name="heading"></slot></h1>
+        <div class="mb-8 py-6 text-white text-center bg-gray-900">
+            <h1 class="text-[28px] my-0 font-semibold leading-none font-headings">
+                <slot name="heading"></slot>
+            </h1>
         </div>
         <div class="px-8">
             <slot />
