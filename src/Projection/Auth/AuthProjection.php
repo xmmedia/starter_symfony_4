b1985d1b5d69f4f2d973023c783ef8f0ca8675fb
Megan <megan.sych@gmail.com>
2024-07-09 18:22:19 -0400

    cs

--- src/Projection/Auth/AuthProjection.php
+++ src/Projection/Auth/AuthProjection.php
@@ -1,6 +1,6 @@
 <?php
 
-declare (strict_types = 1);
+declare(strict_types=1);
 
 namespace App\Projection\Auth;
 
@@ -26,10 +26,9 @@ class AuthProjection implements ReadModelProjection
                         $event->userId()->toString(),
                         $event->createdAt(),
                     );
-
                 },
-
             ]);
+
         return $projector;
     }
 }
