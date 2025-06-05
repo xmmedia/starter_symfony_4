af23c751c2b924c504fdf42a0dc9a8c880d25d58
Darryl Hein <dhein@xmmedia.com>
2025-06-05 15:02:04 -0600

    improve admin dashboard grid alignment

--- public/js/src/admin/admin_dashboard/index.vue
+++ public/js/src/admin/admin_dashboard/index.vue
@@ -2,7 +2,7 @@
     <Teleport to="#header-page-title">Admin</Teleport>
     <Teleport to="#header-actions" />
 
-    <ul class="flex flex-wrap pl-0 list-none">
+    <ul class="flex flex-wrap justify-center sm:justify-start gap-4 pl-0 list-none">
         <li>
             <RouterLink :to="{ name: 'admin-user' }" class="dashboard-link">
                 <AdminIcon icon="users" width="96" height="96" />
