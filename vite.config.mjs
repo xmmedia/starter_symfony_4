92b5f5cf41fe78a6a24b5d27bdf9f2aa53b99d9c
Darryl Hein <dhein@xmmedia.com>
2025-06-05 14:35:35 -0600

    force composer v2 in lando

--- vite.config.mjs
+++ vite.config.mjs
@@ -10,62 +10,64 @@ import dns from 'dns';
 
 dns.setDefaultResultOrder('verbatim');
 
-export default defineConfig({
-    plugins: [
-        mkcert(),
-        vuePlugin(),
-        graphqlPlugin(),
-        manifestSRIPlugin(),
-        symfonyPlugin({
-            refresh: true,
-            sriAlgorithm: 'sha384',
-        }),
-        sentryVitePlugin({
-            disable: process.env.NODE_ENV !== 'production',
-            // @todo-symfony
-            org: 'xm-media',
-            project: 'symfony-starter',
-            telemetry: process.env.NODE_ENV === 'production',
-        }),
-    ],
-    build: {
-        outDir: 'public/build',
-        rollupOptions: {
-            input: {
-                admin: './public/js/src/admin.js',
-                user: './public/js/src/user.js',
-            },
-            output: {
-                manualChunks: {
-                    'vue-final-modal': ['vue-final-modal'],
+export default defineConfig(({ command }) => {
+    return {
+        plugins: [
+            mkcert(),
+            vuePlugin(),
+            graphqlPlugin(),
+            manifestSRIPlugin(),
+            symfonyPlugin({
+                refresh: true,
+                sriAlgorithm: 'sha384',
+            }),
+            sentryVitePlugin({
+                disable: process.env.NODE_ENV !== 'production',
+                // @todo-symfony
+                org: 'xm-media',
+                project: 'symfony-starter',
+                telemetry: process.env.NODE_ENV === 'production',
+            }),
+        ],
+        build: {
+            outDir: 'public/build',
+            rollupOptions: {
+                input: {
+                    admin: './public/js/src/admin.js',
+                    user: './public/js/src/user.js',
+                },
+                output: {
+                    manualChunks: {
+                        'vue-final-modal': [ 'vue-final-modal' ],
+                    },
                 },
             },
+            sourcemap: 'serve' === command,
+            // don't inline assets
+            assetsInlineLimit: 0,
         },
-        sourcemap: true,
-        // don't inline assets
-        assetsInlineLimit: 0,
-    },
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./public/js/src', import.meta.url)),
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./public/js/src', import.meta.url)),
+            },
+            // the default plus .vue
+            extensions: [ '.vue', '.mjs', '.js', '.mts', '.ts', '.jsx', '.tsx', '.json' ],
         },
-        // the default plus .vue
-        extensions: ['.vue', '.mjs', '.js', '.mts', '.ts', '.jsx', '.tsx', '.json'],
-    },
-    css: {
-        devSourcemap: true,
-    },
-    server: {
-        host: true,
-        port: 9008,
-        origin: 'https://localhost:9008',
-        strictPort: true,
-        https: true,
-        watch: {
-            // this is in part needed because the symfony plugin ignores the public dir completely
-            ignored: ['**/vendor/**', '**/var/**'],
+        css: {
+            devSourcemap: true,
         },
-    },
-    appType: 'custom',
-    clearScreen: false,
+        server: {
+            host: true,
+            port: 9008,
+            origin: 'https://localhost:9008',
+            strictPort: true,
+            https: true,
+            watch: {
+                // this is in part needed because the symfony plugin ignores the public dir completely
+                ignored: [ '**/vendor/**', '**/var/**' ],
+            },
+        },
+        appType: 'custom',
+        clearScreen: false,
+    };
 });
