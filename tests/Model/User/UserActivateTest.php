97eca23f4f73526682aff91ebdb8608884d34808
Megan Sych <msych@xmmedia.com>
2025-06-13 09:42:54 -0400

    adding more tests to user

---
+++ tests/Model/User/UserActivateTest.php
@@ -0,0 +1,134 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Model\User;
+
+use App\Model\User\Event;
+use App\Model\User\Exception;
+use App\Model\User\Name;
+use App\Model\User\Role;
+use App\Model\User\User;
+use App\Tests\BaseTestCase;
+use Xm\SymfonyBundle\Model\EmailGatewayMessageId;
+
+class UserActivateTest extends BaseTestCase
+{
+    use UserTestTrait;
+
+    public function testActivateByAdmin(): void
+    {
+        $user = $this->getUser(true, false);
+        $user->deactivateByAdmin();
+        $this->popRecordedEvent($user);
+
+        $user->activateByAdmin();
+        $events = $this->popRecordedEvent($user);
+
+        $this->assertRecordedEvent(
+            Event\UserActivatedByAdmin::class,
+            [],
+            $events,
+        );
+
+        $this->assertCount(1, $events);
+    }
+
+    public function testActivateByAdminAlreadyVerified(): void
+    {
+        $user = $this->getUser(true, false);
+
+        $this->expectException(Exception\InvalidUserActiveStatus::class);
+
+        $user->activateByAdmin();
+    }
+
+    public function testActivateByAdminDeleted(): void
+    {
+        $user = $this->getUserActive();
+        $user->delete();
+
+        $this->expectException(Exception\UserIsDeleted::class);
+        $this->expectExceptionMessage(
+            \sprintf('Tried to activate (by admin) deleted User with ID "%s"', $user->userId()),
+        );
+
+        $user->activateByAdmin();
+    }
+
+
+    public function testActivate(): void
+    {
+        $faker = $this->faker();
+
+        $email = $faker->emailVo();
+        $password = $faker->password();
+        $role = Role::ROLE_USER();
+        $firstName = Name::fromString($faker->firstName());
+        $lastName = Name::fromString($faker->lastName());
+
+        $user = User::addByAdminMinimum(
+            $faker->userId(),
+            $email,
+            $password,
+            $role,
+            $firstName,
+            $lastName,
+            true,
+            $this->userUniquenessCheckerNone,
+        );
+        $this->popRecordedEvent($user);
+
+        $user->activate();
+
+        $events = $this->popRecordedEvent($user);
+
+        $this->assertRecordedEvent(
+            Event\UserActivated::class,
+            [],
+            $events,
+        );
+
+        $this->assertCount(1, $events);
+    }
+
+    public function testActivateAlreadyActivated(): void
+    {
+        $user = $this->getUserActive(false);
+
+        $this->expectException(Exception\UserAlreadyVerified::class);
+
+        $user->activate();
+
+        $events = $this->popRecordedEvent($user);
+
+        $this->assertCount(1, $events);
+    }
+
+    // @todo fix
+    // public function testActivateInvalidUserActiveStatus(): void
+    // {
+    //     $user = $this->getUserInactive(false);
+    //
+    //     $this->expectException(Exception\InvalidUserActiveStatus::class);
+    //
+    //     $user->activate();
+    //
+    //     $events = $this->popRecordedEvent($user);
+    //
+    //     $this->assertCount(1, $events);
+    // }
+
+    public function testActivateDeleted(): void
+    {
+        $user = $this->getUserActive();
+        $user->delete();
+
+        $this->expectException(Exception\UserIsDeleted::class);
+        $this->expectExceptionMessage(
+            \sprintf('Tried to activate deleted User with ID "%s"', $user->userId()),
+        );
+
+        $user->activate();
+    }
+}
