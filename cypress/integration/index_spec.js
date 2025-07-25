6e59c1c3073fd54ea84bd12cb56c8c41f7c5deb7
Darryl Hein <dhein@xmmedia.com>
2025-07-25 13:01:14 -0600

    remove cypress dir
    cypress itself was removed previously

--- cypress/integration/index_spec.js
+++
@@ -1,7 +0,0 @@
-describe('Load Index', function () {
-    it('Does the homepage load', function () {
-        cy.visit('/');
-        cy.get('h1')
-            .contains('Example Domain');
-    });
-});
