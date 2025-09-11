c6433b83ea7250d18318f14e5d231337895d270e
Darryl Hein <dhein@xmmedia.com>
2025-09-11 15:42:29 -0600

    disable console command rector

--- rector.php
+++ rector.php
@@ -52,5 +52,8 @@ return RectorConfig::configure()
         Rector\CodeQuality\Rector\If_\ExplicitBoolCompareRector::class,
         // from set "deadCode"
         Rector\DeadCode\Rector\ClassMethod\RemoveUselessParamTagRector::class,
+        // from Symfony composer set
+        // disabled because it rewrite the console commands to use attributes, but can't get this to work atm
+        Rector\Symfony\Symfony73\Rector\Class_\InvokableCommandInputAttributeRector::class,
     ])
 ;
