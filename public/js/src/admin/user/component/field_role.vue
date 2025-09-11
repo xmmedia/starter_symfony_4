71da07b54acb081fba30a29d5f3844b2f65af386
Darryl Hein <dhein@xmmedia.com>
2025-09-11 10:31:09 -0600

    switch defineModel

--- public/js/src/admin/user/component/field_role.vue
+++ public/js/src/admin/user/component/field_role.vue
@@ -2,13 +2,11 @@
     <div class="field-wrap">
         <label :for="id">Role</label>
 
-        <FieldError v-if="v.$error && v.$invalid">
-            <template v-if="!v.required">
-                A Role is required.
-            </template>
+        <FieldError v-if="v" :v="v">
+            <template #required>A Role is required.</template>
         </FieldError>
 
-        <select :id="id" :value="modelValue" @change="$emit('update:modelValue', $event.target.value)">
+        <select :id="id" v-model="role">
             <option v-for="(name,role) in rootStore.availableRoles" :key="role" :value="role">{{ name }}</option>
         </select>
     </div>
@@ -20,18 +18,16 @@ import { useRootStore } from '@/admin/stores/root';
 
 const rootStore = useRootStore();
 
-defineEmits(['update:modelValue']);
+const role = defineModel({ type: String });
 
 defineProps({
-    modelValue: {
-        type: String,
-        default: null,
-    },
     v: {
         type: Object,
         required: true,
     },
+    id: {
+        type: String,
+        default: () => cuid(),
+    },
 });
-
-const id = cuid();
 </script>
