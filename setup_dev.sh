d0b17648dc4e05b2b60607688ab6c539ab009d36
Darryl Hein <dhein@xmmedia.com>
2025-12-14 21:45:57 -0700

    add comment regarding installing nvm on dev server

--- setup_dev.sh
+++ setup_dev.sh
@@ -47,6 +47,7 @@ touch ~/.ssh/authorized_keys
 chmod 700 ~/.ssh
 chmod 600 ~/.ssh/authorized_keys
 
+# nvm is only installed on dev (just in case it becomes handy at some point)
 echo "Install oh-my-zsh & add nvm vars"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 printf "\nDISABLE_AUTO_TITLE=\"true\"" >> ~/.zshrc
