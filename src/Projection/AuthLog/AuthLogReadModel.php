46ca7b646c6432ccdc2f0175b85d3b490a46ccba
Darryl Hein <dhein@xmmedia.com>
2026-03-24 09:50:18 -0600

    add collation types & add a default value

--- src/Projection/AuthLog/AuthLogReadModel.php
+++ src/Projection/AuthLog/AuthLogReadModel.php
@@ -18,14 +18,14 @@ final class AuthLogReadModel extends AbstractReadModel
         $sql = <<<EOT
 CREATE TABLE `{$tableName}` (
   `auth_log_id` char(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '(DC2Type:uuid)',
-  `event_type` varchar(50) NOT NULL,
+  `event_type` varchar(50) COLLATE utf8mb4_bin NOT NULL,
   `user_id` char(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '(DC2Type:uuid)',
   `impersonated_user_id` char(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '(DC2Type:uuid)',
   `email` varchar(150) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
-  `ip_address` varchar(45) NOT NULL,
-  `user_agent` varchar(500) DEFAULT NULL,
-  `route` varchar(255) DEFAULT NULL,
-  `error_message` varchar(500) DEFAULT NULL,
+  `ip_address` varchar(45) COLLATE utf8mb4_bin NOT NULL,
+  `user_agent` varchar(500) COLLATE utf8mb4_bin DEFAULT NULL,
+  `route` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
+  `error_message` varchar(500) COLLATE utf8mb4_bin DEFAULT NULL,
   `occurred_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 EOT;
@@ -43,7 +43,7 @@ EOT;
         $this->connection->executeQuery($sql);
     }
 
-    protected function insert(array $data, array $types): void
+    protected function insert(array $data, array $types = []): void
     {
         $this->connection->insert(self::TABLE, $data, $types);
     }
