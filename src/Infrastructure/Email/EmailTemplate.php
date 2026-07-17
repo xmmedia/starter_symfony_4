f76a1df926b72aae2f08102a4a5c766d91ae4607
Darryl Hein <dhein@xmmedia.com>
2026-07-17 15:40:20 -0600

    make EmailTemplate class final & add types

--- src/Infrastructure/Email/EmailTemplate.php
+++ src/Infrastructure/Email/EmailTemplate.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 
 namespace App\Infrastructure\Email;
 
-class EmailTemplate
+final readonly class EmailTemplate
 {
-    public const AUTH_USER_INVITE = 'auth-user_invite';
-    public const AUTH_USER_VERIFICATION = 'auth-user_verification';
-    public const AUTH_LOGIN_LINK = 'auth-login_link';
-    public const AUTH_PASSWORD_RESET = 'auth-password_reset';
-    public const USER_PROFILE_UPDATED = 'profile_updated';
-    public const USER_PASSWORD_CHANGED = 'password_changed';
+    public const string AUTH_USER_INVITE = 'auth-user_invite';
+    public const string AUTH_USER_VERIFICATION = 'auth-user_verification';
+    public const string AUTH_LOGIN_LINK = 'auth-login_link';
+    public const string AUTH_PASSWORD_RESET = 'auth-password_reset';
+    public const string USER_PROFILE_UPDATED = 'profile_updated';
+    public const string USER_PASSWORD_CHANGED = 'password_changed';
 }
