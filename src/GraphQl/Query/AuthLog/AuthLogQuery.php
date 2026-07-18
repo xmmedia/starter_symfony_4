db5f572d281867d1f9d296c415c62da3d3df5b4f
Darryl Hein <dhein@xmmedia.com>
2026-07-17 21:17:33 -0600

    add array shape

--- src/GraphQl/Query/AuthLog/AuthLogQuery.php
+++ src/GraphQl/Query/AuthLog/AuthLogQuery.php
@@ -7,6 +7,7 @@ namespace App\GraphQl\Query\AuthLog;
 use App\Entity\AuthLog;
 use App\Model\AuthLog\AuthLogId;
 use App\Projection\AuthLog\AuthLogFinder;
+use JetBrains\PhpStorm\ArrayShape;
 use Overblog\GraphQLBundle\Definition\Resolver\QueryInterface;
 
 final readonly class AuthLogQuery implements QueryInterface
@@ -15,6 +16,7 @@ final readonly class AuthLogQuery implements QueryInterface
     {
     }
 
+    #[ArrayShape([AuthLog::class])]
     public function __invoke(AuthLogId $authLogId): ?AuthLog
     {
         return $this->authLogFinder->find($authLogId);
