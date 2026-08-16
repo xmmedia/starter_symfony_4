d62270f133341b99bbd5f9ba423fad2a01d534e7
Darryl Hein <dhein@xmmedia.com>
2026-08-15 23:11:16 -0600

    remove Stringable interface as php 8 automatically adds it

--- src/Model/Auth/AuthId.php
+++ src/Model/Auth/AuthId.php
@@ -8,7 +8,7 @@ use Xm\SymfonyBundle\Model\UuidId;
 use Xm\SymfonyBundle\Model\UuidInterface;
 use Xm\SymfonyBundle\Model\ValueObject;
 
-final readonly class AuthId implements ValueObject, UuidInterface, \Stringable
+final readonly class AuthId implements ValueObject, UuidInterface
 {
     use UuidId;
 }
