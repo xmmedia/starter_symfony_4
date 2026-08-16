9472091041a668997923bff88d65a53a283e0538
Darryl Hein <dhein@xmmedia.com>
2026-08-15 23:10:15 -0600

    remove array shape on method that has a return type

--- src/GraphQl/Query/AuthLog/AuthLogQuery.php
+++ src/GraphQl/Query/AuthLog/AuthLogQuery.php
@@ -7,7 +7,6 @@ namespace App\GraphQl\Query\AuthLog;
 use App\Entity\AuthLog;
 use App\Model\AuthLog\AuthLogId;
 use App\Projection\AuthLog\AuthLogFinder;
-use JetBrains\PhpStorm\ArrayShape;
 use Overblog\GraphQLBundle\Definition\Resolver\QueryInterface;
 
 final readonly class AuthLogQuery implements QueryInterface
@@ -16,7 +15,6 @@ final readonly class AuthLogQuery implements QueryInterface
     {
     }
 
-    #[ArrayShape([AuthLog::class])]
     public function __invoke(AuthLogId $authLogId): ?AuthLog
     {
         return $this->authLogFinder->find($authLogId);
