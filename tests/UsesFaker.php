14b0e01ea325cbcbd1308ccf56b76b894a9be5ad
Darryl Hein <dhein@xmmedia.com>
2026-08-15 20:48:01 -0600

    make faker calls static so they can be used in phpunit10+ data providers

--- tests/UsesFaker.php
+++ tests/UsesFaker.php
@@ -11,20 +11,24 @@ use Xm\SymfonyBundle\DataFixtures\Faker\Provider as BundleProviders;
 
 trait UsesFaker
 {
-    private Faker\Generator $faker;
+    /** @var Faker\Generator&FakerGenerator */
+    private static Faker\Generator $faker;
 
     /**
+     * Static so that it can also be used from data providers,
+     * which must be static as of PHPUnit 10.
+     *
      * @return Faker\Generator&BundleProviders\AddressFakerProvider&BundleProviders\DateFakerProvider&BundleProviders\EmailFakerProvider&BundleProviders\GenderFakerProvider&BundleProviders\InternetFakerProvider&BundleProviders\NameFakerProvider&BundleProviders\PhoneNumberFakerProvider&BundleProviders\StringFakerProvider&BundleProviders\UuidFakerProvider&Provider\UserFakerProvider&Provider\UuidFakerProvider
      *
      * @phpstan-return Faker\Generator&FakerGenerator
      */
-    protected function faker(): Faker\Generator
+    protected static function faker(): Faker\Generator
     {
-        if (!isset($this->faker)) {
-            $this->faker = self::makeFaker();
+        if (!isset(self::$faker)) {
+            self::$faker = self::makeFaker();
         }
 
-        return $this->faker;
+        return self::$faker;
     }
 
     /**
@@ -36,6 +40,7 @@ trait UsesFaker
     {
         $locales = ['en_CA', 'en_US'];
 
+        /** @var Faker\Generator&FakerGenerator $faker */
         $faker = Faker\Factory::create($locales[array_rand($locales)]);
         $faker->addProvider(new BundleProviders\AddressFakerProvider($faker));
         $faker->addProvider(new BundleProviders\DateFakerProvider($faker));
