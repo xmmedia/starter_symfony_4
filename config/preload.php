e66004c3e1786e2afbf410b7dab176b401c1fda8
Darryl Hein <dhein@xmmedia.com>
2026-09-18 14:18:55 -0600

    update symfony/framework-bundle recipe

--- config/preload.php
+++ config/preload.php
@@ -2,6 +2,6 @@
 
 declare(strict_types=1);
 
-if (file_exists(dirname(__DIR__).'/cache/prod/App_KernelProdContainer.preload.php')) {
-    require dirname(__DIR__).'/cache/prod/App_KernelProdContainer.preload.php';
+foreach (glob(dirname(__DIR__).'/cache/prod/*.preload.php') ?: [] as $file) {
+    require $file;
 }
